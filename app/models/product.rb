75e43d073558a07a28572d0f615ebcbe10595b5d
Kyle Geske <stungeye@gmail.com>
2020-11-16 11:00:17 -0600

    Validates and a seed script for products.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,6 @@
 class Product < ApplicationRecord
+  validates :name, :price_cents, :description, presence: true
+  validates :price_cents, numericality: { only_integer: true }
+
+  has_one_attached :image
 end
