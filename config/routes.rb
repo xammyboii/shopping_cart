811551fc10251cafe4a443eaec2cfc12076d0ded
Michael Bialowas <mbialowas@rrc.ca>
2022-03-16 12:34:41 -0500

    Shopping card and Session implemented with stripe

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
   resources :products, only: %i[index show]
   root to: "products#index"
+
+  # /checkout/create something
+  scope "/checkout" do
+    post "create", to: "checkout#create", as: "checkout_create"
+    get "success", to: "checkout#success", as: "checkout_success"
+    get "cancel", to: "checkout#cancel", as: "checkout_cancel"
+  end
 end
