5dd936478d691b3dd5e3310478e87cae1d29d7c2
xammyboii <xavieryabados.09@gmail.com>
2023-11-01 16:43:51 -0500

    implemented session for products index view

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,9 @@
 class ProductsController < ApplicationController
   def index
     @products = Product.all
+    session[:visit_count] ||= 0
+    session[:visit_count] += 1
+    @visit_count = session[:visit_count]
   end
 
   def show
