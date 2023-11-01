3c36d83624cbef46fa987a18e8e873b433f83e10
xammyboii <xavieryabados.09@gmail.com>
2023-11-01 16:53:55 -0500

    added session into application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  def initialize_session
+    session[:visit_count] ||= 0
+  end
+
+  def increment_visit_count
+    session[:visit_count] += 1
+  end
+
+  def visit_count
+    session[:visit_count]
+  end
 end
